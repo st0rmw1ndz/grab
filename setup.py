1c25ed664f924f6ec19d134f689de84f8e0781aa
st0rm <inthishouseofcards@gmail.com>
2023-12-01 12:22:31 -0500

    try to make it installable

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt") as req:
+    packages = req.read().splitlines()
+
+setuptools.setup(
+    name="grab",
+    version="1.0.0",
+    author="st0rm",
+    author_email="inthishouseofcards@gmail.com",
+    description="simple paste system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/st0rmw1ndz/grab",
+    packages=setuptools.find_packages(),
+    install_requires=packages,
+    include_package_data=True,
+    entry_points={"console_scripts": ["grab=grab:main"]},
+    python_requires=">=3.11",
+)
